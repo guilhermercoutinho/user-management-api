830b429975d56035b5c9ccc3c975f0f8efdebbb8
guilhermercoutinho <guilhermecoutinho.rc@gmail.com>
2024-12-25 20:30:30 -0300

    Alteração nas validações

--- src/middleware/userValidation.js
+++ src/middleware/userValidation.js
@@ -1,76 +1,87 @@
 import Joi from "joi";
 
-const schemaUser = Joi.object({
+// Define o esquema de validação para os dados do usuário
+const schemaUsuario = Joi.object({
   nome: Joi.string().min(3).max(50).required().messages({
-    "string.empty": "O nome é obrigatório",
+    "string.empty": "O nome é obrigatório.",
     "string.min": "O nome deve ter pelo menos 3 caracteres.",
     "string.max": "O nome deve ter no máximo 50 caracteres.",
   }),
   cpf: Joi.string()
     .pattern(/^\d{11}$/)
-    .required()
+    .optional()
     .messages({
-      "string.empty": "o CPF é obrigatório",
-      "string.pattern.base": "O CPF deve conter exatament 11 dígitos",
+      "string.pattern.base": "O CPF deve conter exatamente 11 dígitos.",
     }),
-  email: Joi.string().email().required().messages({
-    "String.empty": "O email é obrigatório.",
-    "String.email": "O email deve ser válido.",
+  email: Joi.string().email().optional().messages({
+    "string.email": "O email deve ser válido.",
   }),
   telefone: Joi.string()
     .pattern(/^\(\d{2}\) \d{4,5}-\d{4}$/)
-    .required()
+    .optional()
     .messages({
-      "string.empty": "O telefone é obrigatório.",
       "string.pattern.base":
         "O telefone deve estar no formato (XX) XXXXX-XXXX.",
     }),
   endereco: Joi.object({
     cep: Joi.string()
       .pattern(/^\d{5}-\d{3}$/)
-      .required()
+      .optional()
       .messages({
-        "string.empty": "O CEP é obrigatório.",
         "string.pattern.base": "O CEP deve estar no formato XXXXX-XXX.",
       }),
-    rua: Joi.string().required().messages({
+    rua: Joi.string().optional().messages({
       "string.empty": "A rua é obrigatória.",
     }),
-    numero: Joi.string().required().messages({
+    numero: Joi.string().optional().messages({
       "string.empty": "O número é obrigatório.",
     }),
-    bairro: Joi.string().required().messages({
+    bairro: Joi.string().optional().messages({
       "string.empty": "O bairro é obrigatório.",
     }),
-    cidade: Joi.string().required().messages({
+    cidade: Joi.string().optional().messages({
       "string.empty": "A cidade é obrigatória.",
     }),
-    estado: Joi.string().length(2).required().messages({
-      "string.empty": "O estado é obrigatório.",
+    estado: Joi.string().length(2).optional().messages({
       "string.length": "O estado deve ter exatamente 2 caracteres.",
     }),
-  }).required(),
+  }).optional(),
   genero: Joi.string()
     .valid("Masculino", "Feminino", "Outro")
     .optional()
     .messages({
       "any.only": "O gênero deve ser Masculino, Feminino ou Outro.",
     }),
-  foto: Joi.string().uri().optional().messages({
-    "string.uri": "A foto deve ser uma URL válida.",
-  }),
 });
 
-const validateUser = (req, res, next) => {
-  const { error } = schemaUser.validate(req.body, { abortEarly: false });
+// Middleware para validação e adição da imagem com base no gênero
+const validarUsuario = async (req, res, next) => {
+  const { error } = schemaUsuario.validate(req.body, { abortEarly: false });
 
   if (error) {
-    const messages = error.details.map((details) => details.message);
-
-    return res.status(400).json({ errors: messages });
+    const mensagens = error.details.map((detalhe) => detalhe.message);
+    return res.status(400).json({ erros: mensagens });
   }
 
-  next();
+  try {
+    // Adiciona a imagem com base no gênero
+    const genero = req.body.genero;
+    const nome = req.body.nome;
+    let foto = "";
+
+    if (genero === "Masculino") {
+      foto = "https://avatar.iran.liara.run/public/boy";
+    } else if (genero === "Feminino") {
+      foto = "https://avatar.iran.liara.run/public/girl";
+    } else {
+      foto = `https://avatar.iran.liara.run/username?username=${nome}`; // Gênero neutro ou outro
+    }
+
+    req.body.foto = foto;
+    next(); // Continua para a próxima middleware ou rota
+  } catch (err) {
+    return res.status(500).json({ erro: "Erro ao definir a foto do usuário." });
+  }
 };
 
-export default validateUser;
+export default validarUsuario;
